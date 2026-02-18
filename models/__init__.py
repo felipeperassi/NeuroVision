41f8c3e5223e1a579767ba30f0dca0a7511bd5b5
felipeperassi <fperassi@fi.uba.ar>
2026-02-18 17:04:39 -0300

    Update model imports to use 'Voxels' terminology for consistency across datasets and architectures

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,4 @@
-from .ae import Roi2LatDataset, Roi2LatAutoencoder # Autoencoder ROI -> Latent Space
-from .mlp_clip import Roi2ClipDataset, Roi2ClipMLP # MLP for ROI -> CLIP
+from .ae import Voxels2LatDataset, Voxels2LatAutoencoder # Autoencoder Voxels -> Latent Space
+from .mlp_clip import Voxels2ClipDataset, Voxels2ClipMLP # MLP for Voxels -> CLIP
 from .mlp_txt import Clip2TxtDataset, Clip2TxtMLP # MLP for CLIP -> Text
-from .cnn_vae import Roi2VaeDataset, Roi2VaeCNN # CNN for ROI -> VAE
+from .cnn_vae import Voxels2VaeDataset, Voxels2VaeCNN # CNN for Voxels -> VAE
